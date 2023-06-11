6818f99a6d793ca5ccd7e95eb0cbfb63eb15f691
VanyaR <vanya.ruzhcov@gmail.com>
2023-06-11 11:55:27 +0300

    отрисовка страницы user добавил дивы , буду подтягивать dev

---
+++ client/src/features/SportsmenPage/SportsmenPage.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function SportsmenPage(): JSX.Element {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+        <div>информация</div>
+      </div>
+      <div>
+        слайдер
+      </div>
+
+    </div>
+  );
+}
+
+export default SportsmenPage;
