4c7e2d108f2727237bfbcd5f1b601dfad1ac6167
Konstantin Mosenkov <mosenkovk@mail.ru>
2023-06-13 12:13:39 +0300

    debag

--- client/src/features/MainPage/Carousel.tsx
+++ client/src/features/MainPage/Carousel.tsx
@@ -51,7 +51,7 @@ const slides: Slide[] = [
   };
 
 
-const goToSlide = (slideIndex:any):void => {
+const goToSlide = (slideIndex:number):void => {
   setCurrentIndex(slideIndex);
 };
 
@@ -70,6 +70,18 @@ const goToSlide = (slideIndex:any):void => {
       <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
         <BsChevronCompactRight onClick={nextSlide} size={30} />
       </div>
+      <div className="flex top-4 justify-center py-2">
+          {slides.map((slide, slideIndex) => (
+            <div
+              key={slideIndex}
+              onClick={() => goToSlide(slideIndex)}
+              className="text-2xl cursor-pointer"
+            >
+              <RxDotFilled />
+            </div>
+          ))}
+        </div>
+        </div>
     </div>
   );
 }
