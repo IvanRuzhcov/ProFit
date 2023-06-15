dc87ef06c08ac424b4e5f4edf18a460a68c77985
VanyaR <vanya.ruzhcov@gmail.com>
2023-06-15 15:36:15 +0300

    Fit: добавил проверку на подписку

--- server/routes/api/subscription.routes.js
+++ server/routes/api/subscription.routes.js
@@ -1,27 +1,25 @@
-const subscriptionRouter = require("express").Router();
-const { Subscription } = require("../../db/models");
+const subscriptionRouter = require('express').Router();
+const { Subscription } = require('../../db/models');
 
-subscriptionRouter.post("/subscribe", async (req, res) => {
+subscriptionRouter.post('/subscribe', async (req, res) => {
   const { userId } = req.body;
-  console.log(req.session.user.id, req.body);
 
   try {
-    // const sub = await Subscription.fineOne({
-    //   where: {
-    //     sportsmen_id: req.session.user.id,
-    //     coach_id: userId,
-    //   },
-    // });
-    // if (!sub) {
-    const subscription = await Subscription.create({
-      sportsmen_id: req.session.user.id,
-      coach_id: userId,
+    const sub = await Subscription.findOne({
+      where: {
+        sportsmen_id: req.session.user.id,
+        coach_id: userId,
+      },
     });
-    res.status(201).json(subscription);
-    // }
+    if (!sub) {
+      const subscription = await Subscription.create({
+        sportsmen_id: req.session.user.id,
+        coach_id: userId,
+      });
+      res.status(201).json(subscription);
+    }
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: "Failed to subscribe" });
+    res.status(500).json({ error: 'Failed to subscribe' });
   }
 });
 
