04de4ec834c96b8901c169891f1261cb353975d4
Maxim <maxbeat040@gmail.com>
2023-07-03 16:10:49 +0300

    Добавлены названия графикам

--- server/middlewares/fileuploadMiddeleware.js
+++ server/middlewares/fileuploadMiddeleware.js
@@ -7,7 +7,7 @@ const storage = async (file) => {
   const size = file.data.length;
   const extension = path.extname(fileName);
 
-  const allowedExtensions = /png|jpeg|jpg|gif|webp|mp4|wmv|mov/;
+  const allowedExtensions = /png|jpeg|JPG|gif|webp|mp4|wmv|mov/;
 
   // eslint-disable-next-line no-throw-literal
   if (!allowedExtensions.test(extension)) throw 'Unsupported extension !';
